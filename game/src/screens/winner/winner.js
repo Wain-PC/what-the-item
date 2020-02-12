cbc071023654998f1b6881821e719648b147ad64
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-02-12 20:51:34 +0300

    Updated UI for 'winner' screen

--- game/src/screens/winner/winner.js
+++ game/src/screens/winner/winner.js
@@ -1,28 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Heading } from "grommet";
+import cn from "classnames";
+import styles from "./winner.module.css";
+import Avatar from "../../components/avatar/avatar";
 
 const Winner = props => {
   const {
-    winner: { name, score }
+    winner: { index, score }
   } = props;
 
   return (
-    <Box fill align="center">
-      <Heading level={1}>Победитель</Heading>
-      <Heading level={2} color="status-ok">
-        {name}
-      </Heading>
-      <Heading level={1}>Набрано очков</Heading>
-      <Heading level={2} color="status-ok">
-        {score}
-      </Heading>
-    </Box>
+    <div className={styles.root}>
+      <Avatar index={index} size="large" />
+      <div
+        className={cn(styles.header, {
+          [styles.winner1]: index === 0,
+          [styles.winner2]: index === 1
+        })}
+      />
+      <div className={styles.scoreContainer}>
+        <div className={styles.score}>{score}</div>
+        <div className={styles.scoreText}>Final Score</div>
+      </div>
+    </div>
   );
 };
 
 Winner.propTypes = {
   winner: PropTypes.shape({
+    index: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     nickName: PropTypes.string.isRequired,
     score: PropTypes.number.isRequired
