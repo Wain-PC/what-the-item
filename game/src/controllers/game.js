e0e34d12ce5ebd65e92c0ba77f1d001dc59d293a
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-05-19 23:37:08 +0300

    Clickable game buttons

--- game/src/controllers/game.js
+++ game/src/controllers/game.js
@@ -1,21 +1,21 @@
 const controller = ({ actions, message }) => {
-  const { type, gamepad, button } = message;
+  const { type, button } = message;
   if (type === "button") {
     switch (button) {
       case "1": {
-        actions.selectAnswer(gamepad, 0);
+        actions.selectAnswer(0);
         break;
       }
       case "2": {
-        actions.selectAnswer(gamepad, 1);
+        actions.selectAnswer(1);
         break;
       }
       case "3": {
-        actions.selectAnswer(gamepad, 2);
+        actions.selectAnswer(2);
         break;
       }
       case "4": {
-        actions.selectAnswer(gamepad, 3);
+        actions.selectAnswer(3);
         break;
       }
       default: {
