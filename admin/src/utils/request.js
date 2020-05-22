7c23639bffbc06fcfd8d420695007bb5c1ac8258
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-05-22 19:50:45 +0300

    Switched clients to POST instead of WS

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -1,68 +1,19 @@
-let socket = null;
-
-const requests = new Map();
-let requestId = 0;
-
-const connect = () => {
-  return new Promise(resolve => {
-    const protocol =
-      window.location.protocol.indexOf("https") === 0 ? "wss" : "ws";
-    socket = new WebSocket(`${protocol}://${window.location.host}/admin`);
-
-    socket.onopen = () => {
-      resolve();
-    };
-
-    socket.onclose = () => {
-      setTimeout(() => {
-        console.log("Реконнект соединения...");
-        connect();
-      }, 1000);
-    };
-
-    socket.onmessage = event => {
-      const { id, payload, error } = JSON.parse(event.data);
-      if (requests.has(id)) {
-        if (error) {
-          requests.get(id).reject(error);
-        } else {
-          requests.get(id).resolve(payload);
-        }
-        requests.delete(id);
-      }
-    };
-
-    socket.onerror = error => {
-      console.log(`Ошибка сокета ${error.message}`);
-    };
-  });
-};
-
 const send = async (path = "", data = {}) => {
-  if (!socket) {
-    await connect();
-  }
-
-  return new Promise((resolve, reject) => {
-    requestId += 1;
-    const request = {
-      id: requestId,
-      path,
-      data,
-      resolve,
-      reject
-    };
-
-    requests.set(requestId, request);
-
-    const payload = {
-      id: requestId,
-      path,
-      data
-    };
-
-    socket.send(JSON.stringify(payload));
+  const response = await fetch(`/admin/${path}`, {
+    method: "POST",
+    body: JSON.stringify(data)
   });
+
+  if (response.ok) {
+    const { payload, error } = await response.json();
+    if (error) {
+      throw new Error(error);
+    }
+
+    return payload;
+  }
+  throw new Error("Unable to contact API");
 };
 
-export { connect, send };
+// eslint-disable-next-line import/prefer-default-export
+export { send };
