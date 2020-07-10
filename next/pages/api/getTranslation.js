8f4d2d5c953649e1603fe2854ad5df27923db9f1
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-07-10 20:35:55 +0300

    Removed console.log

--- next/pages/api/getTranslation.js
+++ next/pages/api/getTranslation.js
@@ -2,7 +2,6 @@ import { receive } from "../../src/utils/request";
 import config from "../../src/utils/config";
 
 export default receive(async () => {
-  console.log(config);
   const { language } = config;
   const data = await import(`../../src/languages/${language}.json`);
   return { language, data };
