68cf3fe0466ccb1d3c1dc0132abbb59d2cfc9cbf
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-07-10 19:06:52 +0300

    Fixed ESLint issues

--- next/src/controllers/top.js
+++ next/src/controllers/top.js
@@ -1,4 +1,4 @@
-const controller = ({ state, actions, message }) => {
+const controller = ({ actions, message }) => {
   // Switch to 'ready' screen 'ok' button is pressed
   if (message.type === "button") {
     switch (message.button) {
